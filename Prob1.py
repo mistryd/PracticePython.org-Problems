1b19c8ed74f883cddaea04f60debbf08b7ed4567
mistryd <mistryd@umich.edu>
2017-05-11 21:08:56 -0400

    Practice problems 1,2,3

---
+++ Prob1.py
@@ -0,0 +1,12 @@
+name = raw_input("What is your name?\n")
+age = int(raw_input("What is your age?\n"))
+
+current_year = 2017
+
+deltaYears = 100 - age
+
+year_100 = current_year + deltaYears
+
+print "\n" + name + ", you will turn 100 in the year " + str(year_100)
+
+
---
+++ Prob2.py
@@ -0,0 +1,6 @@
+num = int(raw_input("Please input any number: "))
+
+if (num % 2) == 0:
+    print "\nYou selected an even number"
+else:
+    print "\nYou selected an odd number"
